93086bff47196e2561436c03eda73922046c4a9b
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-02-07 12:42:52 +0100

    yessss

--- topic_3/type_vector/mpi_type_vector.c
+++ topic_3/type_vector/mpi_type_vector.c
@@ -40,34 +40,30 @@ int Read_vector(int block_length, double* vector, double** slice, int vector_len
   int no_cycles = vector_length / (block_length * comm_sz);
   int cycle_block_length = block_length * comm_sz;
   int slice_length = no_cycles * block_length;
-
-  printf("no_cycles %d, cbl %dm sk %d \n", no_cycles, cycle_block_length, slice_length);
     
   /* define data type */
   MPI_Datatype block_cyclic;
   MPI_Type_vector(no_cycles, block_length, cycle_block_length, MPI_DOUBLE, &block_cyclic);
   MPI_Type_commit(&block_cyclic);
   MPI_Status info;
-
   
   if (my_rank == 0){
     
     for (int i = 1; i < comm_sz;i++){
-      MPI_Send(&vector[i*block_length],1,block_cyclic,i,99,MPI_COMM_WORLD);
+      MPI_Send(&vector[i*block_length],1,block_cyclic,i,99,MPI_COMM_WORLD);      
     }
+    *slice = (double *) calloc(slice_length, sizeof(double));
+    MPI_Sendrecv(&vector[0], 1, block_cyclic, 0, 99, *slice, slice_length, MPI_DOUBLE, 0, 99, MPI_COMM_WORLD, &info);
   } else {
-    double* test = (double*) calloc(slice_length, sizeof(double));
     *slice = (double *) calloc(slice_length, sizeof(double));      
       MPI_Recv(*slice,slice_length,MPI_DOUBLE, 0, 99, MPI_COMM_WORLD, &info);
+  }
 
-      //printf("rank %d %f", my_rank, *(*slice+2));
-      for(int i = 0; i < slice_length;i++){
+  for(int i = 0; i < slice_length;i++){
 	printf("%d/%f ", my_rank, *(*slice+i));
       }
       
       printf("\n");
-    
-  }
   
   return 0;
 }
