509c272c1414fe16eb03e740e2b0ab3614bf9907
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-03-07 18:09:59 +0100

    changes

---
+++ topic_6/daxpy/data_analysis.R
@@ -0,0 +1,7 @@
+setwd("~/git/ParallelProgrammingReports/topic_6/daxpy")
+data<-read.csv("slurm-6330834.txt", header=FALSE, sep=" ")
+data_assembled<-data.frame("cores"=data[1:40,1], "block"=data[which(data[,2]>1),3], "cycle"=data[which(data[,2]==1),3])
+plot(data_assembled[,1], data_assembled[,2:3])
+boxplot(cycle~cores, data=data_assembled, ylim=c(0.002, 0.03), col='red', xlab="cores", ylab="runtime")
+boxplot(block~cores, data=data_assembled, add=TRUE, col='blue')
+legend(0.5,0.03, c("cyclic","block"), lty=c(1,1), lwd=c(5,5),col=c("red", "blue"))
