cd71d2ce4296ad5156241cb9669f90f50c4dd8b9
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-03-09 11:38:18 +0100

    working on report

--- topic_6/tsp/dataanalysis.R
+++ topic_6/tsp/dataanalysis.R
@@ -1,1 +1,3 @@
 setwd("~/git/ParallelProgrammingReports/topic_6/tsp")
+raw_data<-read.csv("slurm-6340294.txt", header=FALSE, sep=" ")
+data<-data.frame("method"=c(rep(1,20), rep(2, 20), rep(3, 20), rep(4,20)), "cores"=raw_data[,1], "runtime"=raw_data[,2])
