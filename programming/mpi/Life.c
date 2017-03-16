c146e6190ff4adbd59ecc8cefd8301e3fd1128ea
Lorenz Gerber <genfys@gmail.com>
2017-03-16 23:21:03 +0800

    code comsetics

--- programming/mpi/Life.c
+++ programming/mpi/Life.c
@@ -1,48 +1,26 @@
-/*******************************************
-MPI Life 1.0
-Copyright 2002, David Joiner and
-  The Shodor Education Foundation, Inc.
-Updated 2010, Andrew Fitz Gibbon and
-  The Shodor Education Foundation, Inc.
-
-A C implementation of Conway's Game of Life.
-
-To run:
-./Life [Rows] [Columns] [Generations] [Display]
-
-See the README included in this directory for
-more detailed information.
-*******************************************/
-
 #include "Life.h"
 #include "Defaults.h" // For Life's constants
 
 int main(int argc, char ** argv) {
-	int count;
-	double start, finish;
-	struct life_t life;
+  int count;
+  double start, finish;
+  struct life_t life;
 
-	init(&life, &argc, &argv);
+  init(&life, &argc, &argv);
 
+  GET_TIME(start);
 
-	GET_TIME(start);
+  for (count = 0; count < life.generations; count++) {
 
-	for (count = 0; count < life.generations; count++) {
+    copy_bounds(&life);
+    eval_rules(&life);
+    update_grid(&life);
+}
 
-		copy_bounds(&life);
+  GET_TIME(finish);
+  printf("%e\n", finish-start);
 
-		eval_rules(&life);
+  cleanup(&life);
+  exit(EXIT_SUCCESS);
 
-		update_grid(&life);
-
-		///throttle(&life);
-	}
-	
-	GET_TIME(finish);
-	
-	printf("%e\n", finish-start);
-       
-	cleanup(&life);
-
-	exit(EXIT_SUCCESS);
 }
